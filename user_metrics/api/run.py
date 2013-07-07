07900b20f0cba6fb26e12f131c7d99d49f2812fc
rfaulk <rfaulkner@wikimedia.org>
2013-07-06 20:37:58 -0700

    mod/add. move handlers outside of apache context.
    Change-Id: I0b6ab2b5f0394b36a9b756fa20441609ab18550c

--- user_metrics/api/run.py
+++ user_metrics/api/run.py
@@ -46,66 +46,13 @@ __license__ = "GPL (version 2 or later)"
 import multiprocessing as mp
 
 from user_metrics.config import logging, settings
-from user_metrics.api.engine.request_manager import job_control, \
-    requests_notification_callback
-from user_metrics.api.engine.response_handler import process_responses
 from user_metrics.api.views import app
-from user_metrics.api.engine.request_manager import api_request_queue, \
-    req_notification_queue_out, req_notification_queue_in, api_response_queue
-from user_metrics.utils import terminate_process_with_checks
-
-job_controller_proc = None
-response_controller_proc = None
-rm_callback_proc = None
-
-
-######
-#
-# Define Custom Classes
-#
-#######
-
-
-def teardown():
-    """ When the instance is deleted store the pickled data and shutdown
-        the job controller """
-
-    # Try to shutdown the job control proc gracefully
-    try:
-        terminate_process_with_checks(job_controller_proc)
-        terminate_process_with_checks(response_controller_proc)
-        terminate_process_with_checks(rm_callback_proc)
-
-    except Exception:
-        logging.error(__name__ + ' :: Could not shut down callbacks.')
-
-
-def setup_controller(req_queue, res_queue, msg_queue_in, msg_queue_out):
-    """
-        Sets up the process that handles API jobs
-    """
-    job_controller_proc = mp.Process(target=job_control,
-                                     args=(req_queue, res_queue))
-    response_controller_proc = mp.Process(target=process_responses,
-                                          args=(res_queue,
-                                                msg_queue_in))
-    rm_callback_proc = mp.Process(target=requests_notification_callback,
-                                  args=(msg_queue_in,
-                                        msg_queue_out))
-    job_controller_proc.start()
-    response_controller_proc.start()
-    rm_callback_proc.start()
 
 ######
 #
 # Execution
 #
 #######
-
-# initialize API data - get the instance
-
-setup_controller(api_request_queue, api_response_queue,
-                 req_notification_queue_in, req_notification_queue_out)
 
 app.config['SECRET_KEY'] = settings.__secret_key__
 app.config['VERSION'] = settings.version
@@ -141,10 +88,7 @@ if settings.__flask_login_exists__:
 
 
 if __name__ == '__main__':
-    try:
-        app.run(debug=True,
-                use_reloader=False,
-                host=settings.__instance_host__,
-                port=settings.__instance_port__,)
-    finally:
-        teardown()
+    app.run(debug=True,
+            use_reloader=False,
+            host=settings.__instance_host__,
+            port=settings.__instance_port__,)
---
+++ user_metrics/api/run_handlers.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+This module handle the exescution of the job handler components of the User
+metrics API.  This piece consists of three components:
+
+    Job Controller:
+        Schedules incoming requests for execution.
+
+    Response Handler:
+        Synthesizes responses from completed jobs.
+
+    Request Notification Callback:
+        Handles sending notifications on job status.
+"""
+
+from user_metrics.api.engine.response_handler import process_responses
+from user_metrics.api.engine.request_manager import api_request_queue, \
+    req_notification_queue_out, req_notification_queue_in, api_response_queue
+from user_metrics.api.engine.request_manager import job_control, \
+    requests_notification_callback
+from user_metrics.api.engine.response_handler import process_responses
+from user_metrics.utils import terminate_process_with_checks
+
+def setup_controller(req_queue, res_queue, msg_queue_in, msg_queue_out):
+    """
+        Sets up the process that handles API jobs
+    """
+    job_controller_proc = mp.Process(target=job_control,
+                                     args=(req_queue, res_queue))
+    response_controller_proc = mp.Process(target=process_responses,
+                                          args=(res_queue,
+                                                msg_queue_in))
+    rm_callback_proc = mp.Process(target=requests_notification_callback,
+                                  args=(msg_queue_in,
+                                        msg_queue_out))
+    job_controller_proc.start()
+    response_controller_proc.start()
+    rm_callback_proc.start()
+
+
+def teardown():
+    """ When the instance is deleted store the pickled data and shutdown
+        the job controller """
+
+    # Try to shutdown the job control proc gracefully
+    try:
+        terminate_process_with_checks(job_controller_proc)
+        terminate_process_with_checks(response_controller_proc)
+        terminate_process_with_checks(rm_callback_proc)
+
+    except Exception:
+        logging.error(__name__ + ' :: Could not shut down callbacks.')
+
