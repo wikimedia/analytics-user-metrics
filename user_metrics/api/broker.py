44d16d741c2a2e5f6caa74a5634e2b7970d2ee3e
rfaulk <rfaulkner@wikimedia.org>
2013-07-08 22:09:39 -0700

    add. FileBroker definition.
    Change-Id: I2aa2ef18d590f357baf8285f6a0340b538912254

--- user_metrics/api/broker.py
+++ user_metrics/api/broker.py
@@ -10,6 +10,10 @@ __date__ = "2013-07-06"
 __license__ = "GPL (version 2 or later)"
 
 
+import json
+import os
+
+
 class Broker(object):
     """
     Base class for broker
@@ -22,10 +26,49 @@ class Broker(object):
         raise NotImplementedError()
 
     def add(self, target, key, value):
+        """
+        Add a key/value pair to the broker
+        """
         raise NotImplementedError()
 
     def remove(self, target, key):
+        """
+        Remove a key/value pair to the broker
+        """
         raise NotImplementedError()
 
     def get(self, target, key):
+        """
+        Retrieve a key/value pair to the broker
+        """
         raise NotImplementedError()
+
+
+class FileBroker(Broker):
+    """
+    Implements a broker that uses a flat file as a broker
+    """
+
+    def __init__(self, **kwargs):
+        super(FileBroker, self).__init__(**kwargs)
+
+    def compose(self):
+        pass
+
+    def add(self, target, key, value):
+        """
+        Adds key/value pair
+        """
+        if os.path.isfile(target):
+            mode = 'a'
+        else:
+            mode = 'w'
+
+        with open(target, mode) as f:
+            f.write(json.dumps({key: value}) + '\n')
+
+    def remove(self, target, key):
+        pass
+
+    def get(self, target, key):
+        pass
