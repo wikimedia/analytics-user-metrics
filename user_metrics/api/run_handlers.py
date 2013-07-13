7d245a1714ba2e3fe38f7b2639b961a181d5612b
rfaulk <rfaulkner@wikimedia.org>
2013-07-12 20:40:26 -0700

    mod. replace response queue with a FileBroker target..
    Change-Id: I2e26e06d030e2b4d060cd073253d5c4dc8c942ef

--- user_metrics/api/run_handlers.py
+++ user_metrics/api/run_handlers.py
@@ -23,7 +23,7 @@ __license__ = "GPL (version 2 or later)"
 
 import multiprocessing as mp
 from user_metrics.api.engine.request_manager import \
-    req_notification_queue_out, req_notification_queue_in, api_response_queue
+    req_notification_queue_out, req_notification_queue_in
 from user_metrics.api.engine.response_handler import process_responses
 from user_metrics.api.engine.request_manager import job_control, \
     requests_notification_callback
@@ -35,15 +35,13 @@ response_controller_proc = None
 rm_callback_proc = None
 
 
-def setup_controller(res_queue, msg_queue_in, msg_queue_out):
+def setup_controller(msg_queue_in, msg_queue_out):
     """
         Sets up the process that handles API jobs
     """
-    job_controller_proc = mp.Process(target=job_control,
-                                     args=res_queue)
+    job_controller_proc = mp.Process(target=job_control)
     response_controller_proc = mp.Process(target=process_responses,
-                                          args=(res_queue,
-                                                msg_queue_in))
+                                          args=msg_queue_in)
     rm_callback_proc = mp.Process(target=requests_notification_callback,
                                   args=(msg_queue_in,
                                         msg_queue_out))
@@ -67,6 +65,5 @@ def teardown():
 
 
 if __name__ == '__main__':
-    setup_controller(api_response_queue,
-        req_notification_queue_in,
+    setup_controller(req_notification_queue_in,
         req_notification_queue_out)
