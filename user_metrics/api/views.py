167eba5500f7bebabf74d8c1ff0e06231479c890
rfaulk <rfaulkner@wikimedia.org>
2013-08-19 23:44:01 -0700

    add - RegexConverter for url mapping.
    Change-Id: I9077957f1050aea7011e2c9c8ad012deb540634c

--- user_metrics/api/views.py
+++ user_metrics/api/views.py
@@ -542,6 +542,15 @@ view_list = {
 }
 
 # Dict stores routing paths for each view
+
+from werkzeug.routing import BaseConverter
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+app.url_map.converters['regex'] = RegexConverter
+
 route_deco = {
     api_root.__name__: app.route('/'),
     all_urls.__name__: app.route('/all_requests'),
